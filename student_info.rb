c877a98417ab08f9bdaca5d0442338d28ceb17ae
TinaLoh <tinalola@gmail.com>
2014-10-31 13:14:10 -0700

    collections_studentinfo2

---
+++ student_info.rb
@@ -0,0 +1,64 @@
+student_info = [
+
+    {first_name: 'John',
+    last_name: 'Foley',
+    email: 'john@gschool.it',
+    class: 'Beginning snark'},
+
+
+    {first_name: 'Sylwester',
+    last_name: 'Kelsey',
+    email: 'sellie@gmail.com',
+    class: 'Ruby Immersive'},
+
+
+    {first_name: 'Timothy',
+    last_name: 'Rama ',
+    email: 'tim.rama@gmail.com ',
+    class: 'Ruby Immersive'},
+
+    {first_name: 'Kane',
+    last_name: 'Baccigalupi',
+    email: 'kane@gschool.it',
+    class: 'C for dummies'},
+
+
+    {first_name: 'Nikita',
+    last_name: 'Theodosius',
+    email: 'nikita.theo@gmail.com',
+    class: 'Ruby Immersive'},
+
+    {first_name: 'Roddy',
+    last_name: 'Eldred',
+    email: 'roddy.el@gmail.com',
+    class: 'Ruby Immersive'},
+
+    {first_name: 'Martha',
+    last_name: 'Berner',
+    email: 'martha@gschool.it',
+    class: 'Time travel for beginners'},
+
+    {first_name: 'Kofi',
+    last_name: 'Thomas',
+    email: 'k.thomas@hotmail.com',
+    class: 'Ruby Immersive'}
+    ]
+
+    puts "All"
+
+    student_info.each do |x|
+      puts "#{x[:last_name]} #{x[:first_name]} #{x[:email]}"
+    end
+
+
+
+    puts "Some"
+
+    student_info.each do |x|
+      last_name = x[:last_name]
+      last_name_with_tT = last_name.include? 'T' || 't'
+      # if x[:last_name].include? 'T' || 't'
+      if last_name_with_tT
+        puts "#{x[:last_name]} #{x[:first_name]} #{x[:email]}"
+      end
+    end
